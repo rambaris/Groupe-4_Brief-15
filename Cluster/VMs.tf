5a4120485d620bca7f5c62e956033fdf5cdd2819
cedric69lt <127845795+cedric69lt@users.noreply.github.com>
2023-07-10 10:13:12 +0200

    Update VMs.tf

--- Cluster/VMs.tf
+++ Cluster/VMs.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "Kubernetes" {
-   name = "Hocine-Kubernetes-RG"
+   name = "Groupe4-Brief15-RG"
    location = var.location
 }
 
@@ -11,7 +11,7 @@ resource "tls_private_key" "SSH" {
 #### 2 Workers VM
  resource "azurerm_linux_virtual_machine" "test" {
     count                 = 2
-    name                  = "Worker${count.index}"
+    name                  = "worker${count.index}"
     location              = azurerm_resource_group.Kubernetes.location
     resource_group_name   = azurerm_resource_group.Kubernetes.name
     size                  = "Standard_D2ds_v4"
@@ -19,19 +19,19 @@ resource "tls_private_key" "SSH" {
      azurerm_network_interface.test["${count.index}"].id
     ]
 
-    source_image_reference  {
-      publisher = "Canonical"
-      offer     = "0001-com-ubuntu-server-jammy"
-      sku       = "22_04-lts-gen2"
-      version   = "latest"
-    }
+    storage_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "8.0"
+    version   = "latest"
+  }
 
-    computer_name                   = "KUB-Worker-${count.index}"
-    admin_username                  = "hocine"
+    computer_name                   = "worker-${count.index}"
+    admin_username                  = "group4"
     disable_password_authentication = true
 
     admin_ssh_key {
-      username   = "hocine"
+      username   = "group4"
       public_key = tls_private_key.SSH.public_key_openssh
    }
 
@@ -43,7 +43,7 @@ resource "tls_private_key" "SSH" {
 }
 
  resource "azurerm_linux_virtual_machine" "Manager" {
-    name                  = "Manager"
+    name                  = "manager"
     location              = azurerm_resource_group.Kubernetes.location
     resource_group_name   = azurerm_resource_group.Kubernetes.name
     size                  = "Standard_D2ds_v4"
@@ -51,19 +51,19 @@ resource "tls_private_key" "SSH" {
      azurerm_network_interface.test["${2}"].id
      ]
 
-    source_image_reference {
-     publisher = "Canonical"
-     offer     = "0001-com-ubuntu-server-jammy"
-     sku       = "22_04-lts-gen2"
-     version   = "latest"
-    }
+    storage_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "8.0"
+    version   = "latest"
+  }
 
-    computer_name                   = "KUB-Manager"
-    admin_username                  = "hocine"
+    computer_name                   = "manager"
+    admin_username                  = "group4"
     disable_password_authentication = true
 
     admin_ssh_key {
-     username   = "hocine"
+     username   = "group4"
      public_key = tls_private_key.SSH.public_key_openssh
     }
 
