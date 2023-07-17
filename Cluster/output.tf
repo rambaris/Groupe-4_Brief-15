8c4dd7c03da0ae96abbe2e9d51255a9bdf0bfb15
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-07-17 12:11:58 +0200

    Update output.tf

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -6,3 +6,14 @@ output "pub" {
 output "private" {
   value = tls_private_key.SSH.private_key_pem
 }
+output "IP_pub_worker_0" {
+  value = azurerm_public_ip.test[0].ip_address
+}
+
+output "IP_pub_worker_1" {
+  value = azurerm_public_ip.test[1].ip_address
+}
+
+output "IP_pub_manager" {
+  value = azurerm_public_ip.test[2].ip_address
+}
