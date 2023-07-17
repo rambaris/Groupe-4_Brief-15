5038bb93ba22b5ff6ef29a5929cedcc54e13ce98
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-07-17 10:28:04 +0200

    Update output.tf

--- Main/output.tf
+++ Main/output.tf
@@ -7,3 +7,15 @@ output "Cle_privee" {
     value   = module.ModuleEnfant.private
     sensitive = true
 }
+
+output "IP_worker_0" {
+  value = module.ChildResources.IP_pub_worker_0
+}
+
+output "IP_worker_1" {
+  value = module.ChildResources.IP_pub_worker_1
+}
+
+output "IP_manager" {
+  value = module.ChildResources.IP_pub_manager
+}
