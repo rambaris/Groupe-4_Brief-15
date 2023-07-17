9bed7c424ecdad8397f9a5ee0b6c59da332e79b7
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-07-17 12:04:04 +0200

    Update outputsTFversinventaire.sh

--- Main/outputsTFversinventaire.sh
+++ Main/outputsTFversinventaire.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+terraform output -raw Cle_privee > id_rsa
+sudo chmod 400 id_rsa
+sudo echo -e "[masters]\n\nmaster ansible_host=$(terraform output -raw IP_manager) ansible_user=momo\nmaster ansible_ssh_private_key_file=id_rsa\n\n[workers]\n\nworker0 ansible_host=$(terraform output -raw IP_worker_0) ansible_user=momo\nworker0 ansible_ssh_private_key_file=id_rsa\n\nworker1 ansible_host=$(terraform output -raw IP_worker_1) ansible_user=momo\nworker1 ansible_ssh_private_key_file=id_rsa" > inventaire.ini
